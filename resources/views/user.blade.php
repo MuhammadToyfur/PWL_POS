28cdfbbb9461e940d4e27c6614dc77e1f69b519d
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-18 22:15:23 +0700

    Pertemuan 4 - Praktikum 2.1

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -13,14 +13,13 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @foreach ($data as $d)
             <tr>
-                <th>{{ $d->user_id }}</th>
-                <th>{{ $d->username }}</th>
-                <th>{{ $d->nama }}</th>
-                <th>{{ $d->level_id }}</th>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
-            @endforeach
+          
         </table>
     </body>
 </html>
