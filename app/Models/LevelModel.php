738788bf68c9b460a63e2bb2fd9165c9f559306e
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-25 07:48:33 +0700

    Pertemuan 4 - Praktikum 2

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+
+
+    public function level(): BelongsTo {
+        return $this->belongsTo(UserModel::class);
+}
+}
+
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,19 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
-
-   
     protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
+    protected $primarykey = 'user_id';
 
     /**
-     * The attributes that are mass assignable.
-     *
+     * 
      * @var array
      */
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+     //protected $fillable = ['level_id', 'username', 'nama'];    
+     
+     public function level(): BelongsTo
+     {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+     }
 }
