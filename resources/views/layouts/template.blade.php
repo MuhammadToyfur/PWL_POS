8acb6b90929c9d2acfc31fa1d5ef5afbdb4e5888
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-09-25 11:22:38 +0700

    Pertemuan 5 - Praktikum 2

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -8,8 +8,10 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ assets('plugins/fontawesome-free/css/all.min.css')}}">
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
+  <link rel="stylesheet" href="{{ assets('dist/css/adminlte.min.css')}}">
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css')}}">
 </head>
 <body class="hold-transition sidebar-mini">
@@ -23,6 +25,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url ('/')}}" class="brand-link">
+      <img src="{{ assets('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <img src="{{ asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
