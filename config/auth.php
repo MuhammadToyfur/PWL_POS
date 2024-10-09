4e696373f58441461fb515bc6b3946084bd79692
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-10-09 10:24:40 +0700

    Pertemuan 7 - Praktikum 1

--- config/auth.php
+++ config/auth.php
@@ -59,10 +59,11 @@ return [
     |
     */
 
-    'providers' => [
+ 'providers' => [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+            'model' => App\Models\UserModel::class,
         ],
 
         // 'users' => [
